9f7631bb75d52e4d5535ee62ac89af5ca054dddf
Dimitar Tasev <dimtasev@gmail.com>
2017-06-07 12:27:49 +0100

    ff

--- py/cupy/loop2d.py
+++ py/cupy/loop2d.py
@@ -2,6 +2,10 @@ import cupy as cp
 
 
 def loop2d(data, kernel=3):
+    """
+    This should be doing a row-major traversal.
+    The data is broadcast into a 1D array when transferred to the device.
+    """
     f = cp.ElementwiseKernel(
         in_params='raw T in, int32 width, int32 kernel, int32 kernel_width',
         out_params='raw T out',
@@ -21,5 +25,8 @@ def loop2d(data, kernel=3):
         return data
 
     kernel_width = (kernel - 1) / 2
-
-    return f(data, data.shape[0], kernel, kernel_width, size=data.shape[1])
+    # this is for the outer for loop, looping over the rows
+    data_height = data.shape[1]
+    # this is for the inner loop, looping over the columns
+    data_width = data.shape[0]
+    return f(data, data_width, kernel, kernel_width, size=data_height)

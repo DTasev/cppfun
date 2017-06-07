abc49a3923cb0813bd7af308752bcb850465daa6
Dimitar Tasev <dimtasev@gmail.com>
2017-06-07 13:51:16 +0100

    added loop2d, and allocate output manually in median1d

--- py/cupy/loop2d.py
+++ py/cupy/loop2d.py
@@ -6,27 +6,34 @@ def loop2d(data, kernel=3):
     This should be doing a row-major traversal.
     The data is broadcast into a 1D array when transferred to the device.
     """
+    # allocate memory for the output parameter
+    data_out = cp.zeros(data.shape, dtype=data.dtype)
     f = cp.ElementwiseKernel(
         in_params='raw T in, int32 width, int32 kernel, int32 kernel_width',
-        out_params='raw T out',
+        out_params='T out',
         preamble=r"""
         #include <stdio.h>
         """,
         operation=r"""
+        printf("image width: %d", width);
+        int col = i / width;
         for(int j = 0; j < width; ++j){
-            int pos = i*j+j;
+            int pos = col * j + j;
             printf("value at %d: %d\n", pos, in[pos]);
+            out = in[pos] + 3;
         }
         """,
         name='loop2d',
-        options=('-std=c++11',)
+        options=('-std=c++11',),
+        reduce_dims=False
     )
     if kernel <= 1:
         return data
 
     kernel_width = (kernel - 1) / 2
     # this is for the outer for loop, looping over the rows
-    data_height = data.shape[1]
+    # data_height = data.shape[1]
     # this is for the inner loop, looping over the columns
     data_width = data.shape[0]
-    return f(data, data_width, kernel, kernel_width, size=data_height)
+    # we pass in data_out as the output parameter
+    return f(data, data_width, kernel, kernel_width, data_out)
--- py/cupy/median1d.py
+++ py/cupy/median1d.py
@@ -8,18 +8,19 @@ def median1d(data, kernel=3):
     we need to specify the size of the outer for loop with size=data.size in the
     function call
     """
+    data_out = cp.zeros(data.shape, dtype=data.dtype)
     f = cp.ElementwiseKernel(
         in_params='raw T in, int32 kernel, int32 kernel_width',
-        out_params='raw T out',
+        out_params='T out',
         operation=r"""
         if(i > kernel_width && i < _ind.size() - kernel_width){
             float total = 0;
             for(int ki = -kernel_width; ki <= kernel_width; ++ki){
                 total += in[i+ki];
             }
-            out[i] = (total / kernel);
+            out = (total / kernel);
         }else{
-            out[i] = in[i];
+            out = in[i];
         }
         """,
         name='median1d',
@@ -30,4 +31,4 @@ def median1d(data, kernel=3):
 
     kernel_width = (kernel - 1) / 2
 
-    return f(data, kernel, kernel_width, size=data.size)
+    return f(data, kernel, kernel_width, data_out)

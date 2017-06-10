80c8b67998335ea4672d9fb729183760a378f391
Dimitar Tasev <dbt@aber.ac.uk>
2017-06-10 20:00:59 +0100

    Finally figured out how to pass a mutable reference lol

--- rust/game-of-life/src/cell.rs
+++ rust/game-of-life/src/cell.rs
@@ -1,10 +1,11 @@
-const NEIGHBOURS_TO_LIVE: u32  = 2;
-const NEIGHBOURS_TO_PROCREATE: u32  = 3;
-const NEIGHBOURS_TO_DIE: u32  = 4;
+const NEIGHBOURS_TO_LIVE: u32 = 2;
+const NEIGHBOURS_TO_PROCREATE: u32 = 3;
+const NEIGHBOURS_TO_DIE: u32 = 4;
 
-#[derive(Debug)]
-pub enum Cell{
-    Alive { x: i32, y: i32, neighbours: i32 },
-    Baby { x: i32, y: i32, neighbours: i32 },
-    Empty
+#[derive(Debug, Copy, Clone)]
+pub enum Cell {
+    Alive { x: usize, y: usize, neighbours: i32 },
+    Baby { x: usize, y: usize, neighbours: i32 },
+    Empty,
 }
+
--- rust/game-of-life/src/main.rs
+++ rust/game-of-life/src/main.rs
@@ -6,28 +6,57 @@ use cell::Cell;
 const NUM_CELLS: usize = 5;
 
 
-pub fn handle_neighbours(x: &i32, y: &i32, neighbours: &i32, mut babyq: &VecDeque<Cell>, mut deathq: &VecDeque<Cell>){
-    for row in x-1..x+1{ // is it only till x+1?
-        for col in y-1..y+1{
-            // nope this doesnt work
+pub fn handle_neighbours(x: usize,
+                         y: usize,
+                         pneighbours: &mut i32,
+                         babyq: &mut VecDeque<Cell>,
+                         grid: &mut [[cell::Cell; NUM_CELLS]; NUM_CELLS]) {
+
+    for row in x - 1..x + 1 {
+        // is it only till x+1?
+        for col in y - 1..y + 1 {
+            match grid[row][col] {
+                Cell::Alive { x, y, neighbours } => {
+                    // if self then skip
+                    if row == x && col == y {
+                        continue;
+                    } else {
+                        *pneighbours += 1;
+                    }
+                }
+                Cell::Baby { x, y, neighbours } => {}
+                Cell::Empty {} => {}
+            }
         }
     }
 }
 
 fn main() {
     let mut cellq: VecDeque<Cell> = VecDeque::with_capacity(NUM_CELLS);
-    cellq.push_back(Cell::Alive {x: 3, y: 4, neighbours: 0});
+    cellq.push_back(Cell::Alive {
+                        x: 3,
+                        y: 4,
+                        neighbours: 0,
+                    });
 
     let mut babyq: VecDeque<Cell> = VecDeque::with_capacity(NUM_CELLS);
 
-    let grid = [[0; NUM_CELLS]; NUM_CELLS]; // 2D array NUM_CELLSxNUM_CELLS
+    let mut grid = [[Cell::Empty; NUM_CELLS]; NUM_CELLS]; // 2D array NUM_CELLS x NUM_CELLS
 
-    // while let Some(c) = cellq.pop_front() {
-    //     match c{
-    //         Cell::Alive{x, y, neighbours} => {
-    //             handle_neighbours(&x, &y, &neighbours, &babyq, &deathq);
-    //         },
-    //         Cell::Baby{x, y, neighbours} => panic!("Cell Queue must not have anything other than cells"),
-    //     }
-    // }
+    while let Some(c) = cellq.pop_front() {
+        match c {
+            Cell::Alive {
+                x,
+                y,
+                mut neighbours,
+            } => {
+                handle_neighbours(x, y, &mut neighbours, &mut babyq, &mut grid);
+            }
+            Cell::Baby { x, y, neighbours } => {
+                panic!("Cell Queue must not have anything other than cells")
+            }
+            Cell::Empty {} => {}
+        }
+    }
 }
+

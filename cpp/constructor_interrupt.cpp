173ad11828d1cf03a0ef69b844a652b154ebe0ab
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2017-06-30 10:18:16 +0100

    cpp things

---
+++ cpp/constructor_interrupt.cpp
@@ -0,0 +1,27 @@
+#include <memory>
+#include <stdio.h>
+
+struct A {
+
+  A(int *ff) {
+    ff = new int(300);
+    some_memory = ff;
+    throw new std::runtime_error("omg");
+  }
+
+  ~A() {
+    delete some_memory;
+    printf("I have been destroyed");
+  }
+  int *some_memory = nullptr;
+};
+
+int main() {
+  int *ff = nullptr;
+  try {
+    A a(ff);
+  } catch (...) {
+    printf("we leaked\n%d", ff);
+  }
+  printf("we didnt throw\n%d", *ff);
+}
